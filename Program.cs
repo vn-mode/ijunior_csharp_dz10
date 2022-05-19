c383feeb049a6574757044325a7a39add949e4d0
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-05-19 04:10:34 +0300

    dz10
    dz10

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace vn_mode_csharp_dz10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingNumber = 5;
+            int endNumber = 96;
+            int ourStep = 7;
+
+            for (int i = startingNumber; i <= endNumber; i += ourStep)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
